1cdbf84e3c58ba94ceb2c6888cce7a24081cd6f7
AxelFridman <fridman.axel@gmail.com>
2022-03-26 15:19:51 -0300

    Se creo el archivo cpp, esta vacio

---
+++ tpEjercicio1Version1/cmake-build-debug/CMakeFiles/tpEjercicio1Version1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tpEjercicio1Version1.dir/main.cpp.o"
+  "tpEjercicio1Version1"
+  "tpEjercicio1Version1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tpEjercicio1Version1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
