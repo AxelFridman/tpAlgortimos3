074ac762ab915e1ac89b7cd9e2abe460a3d19ef1
AxelFridman <fridman.axel@gmail.com>
2022-03-27 20:38:07 -0300

    Escribi un hola mundo

--- tpEjercicio1Version1/main.cpp
+++ tpEjercicio1Version1/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
-
+#include <fstream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+#include <set>
+using namespace std;
+void visualizarVector(const vector<int> v){
+    for(int i=0;i<v.size();i++){
+        std :: cout << " "<<v[i] ;
+    }
+    std :: cout << " "<< endl ;
+}
+void visualizarMatriz(vector<vector<int> > v){
+    std :: cout << "--------"<<endl ;
+    for(int i=0;i<v.size();i++){
+        visualizarVector(v[i]) ;
+    }
+    std :: cout << "--------"<<endl ;
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    tuple<int, string> tup = make_tuple(13,"hola prueba");
+    std::cout << get<1>(tup) << std::endl;
+    std::cout << "Hola mundo" << std::endl;
     return 0;
 }
