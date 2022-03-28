ba7d164959ee6d22afb2280d98d68b7698540eb8
juan <juanignacio.lin@gmail.com>
2022-03-27 21:00:28 -0300

    Estamos agregando el archivo de juan: funcion del ejercicio 1 sin niguna poda

--- tpEjercicio1Version1/main.cpp
+++ tpEjercicio1Version1/main.cpp
@@ -1,27 +1,21 @@
 #include <iostream>
-#include <fstream>
 #include <vector>
-#include <tuple>
-#include <algorithm>
-#include <set>
+#include "red.h"
+#include "TP-1.h"
 using namespace std;
-void visualizarVector(const vector<int> v){
-    for(int i=0;i<v.size();i++){
-        std :: cout << " "<<v[i] ;
-    }
-    std :: cout << " "<< endl ;
-}
-void visualizarMatriz(vector<vector<int> > v){
-    std :: cout << "--------"<<endl ;
-    for(int i=0;i<v.size();i++){
-        visualizarVector(v[i]) ;
-    }
-    std :: cout << "--------"<<endl ;
-}
-int main() {
-    tuple<int, string> tup = make_tuple(13,"hola prueba");
-    std::cout << get<1>(tup) << std::endl;
-    std::cout << "Hola mundo" << std::endl;
-    std::cout << "comoe stas" << std::endl;
-    return 0;
+
+int main(int argc, char** argv){
+
+    /*if(argc!=2){
+            cout << "Esta mal" << endl;
+            return 0;
+    }*/
+
+    char* archivo;
+    cout << "Entrar archivo: ";
+    cin >> archivo;
+
+    //Red lared(argv[1]);
+    Red lared(archivo);
+    ejercicio1(lared);
 }
