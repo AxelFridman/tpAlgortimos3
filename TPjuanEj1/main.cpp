85240e043af4df6d74984fe639a285d6bbaf41ea
Noe Hsueh <noehsueh@gmail.com>
2022-04-07 21:32:23 -0300

    código de pd, falta emprolijar, chequear complejidades, bu

--- TPjuanEj1/main.cpp
+++ TPjuanEj1/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include "red.h"
 #include "tp1.h"
+#include "actividades_PD.h"
 using namespace std;
 
 int main(int argc, char** argv){
@@ -14,8 +15,11 @@ int main(int argc, char** argv){
 
     //Red lared(argv[1]);
 
-    Red lared(archivo);
-    //Red lared("../instancias/brock200_2.clq");
-    ejercicio1(lared);
+//    Red lared(archivo);
+//    //Red lared("../instancias/brock200_2.clq");
+//    ejercicio1(lared);
 
+    Actividades Actividades(archivo);
+    cout << Actividades.ejer3_4() << endl;
+    Actividades.mostrarSolucion();
 }
